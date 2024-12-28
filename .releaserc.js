15501bd738ec0ac0d28a5624f1755d94da1fc758
Felipe Pimentel <fpimentel88@gmail.com>
2024-12-28 15:51:58 -0300

    chore: update release configuration and workflow for semantic-release
    - Refactored the .releaserc.js to utilize environment variables for MODULE_NAME and MODULE_PATH, enhancing flexibility and consistency.
    - Modified the GitHub Actions release workflow to set environment variables dynamically before running semantic-release, improving the integration process.
    - Updated the changelog file path and release message format to ensure they align with the new environment variable usage, streamlining the release process.

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: ["main"],
-  tagFormat: "${MODULE_NAME}/v${version}",
+  tagFormat: `${process.env.MODULE_NAME}/v${process.env.npm_package_version}`,
   plugins: [
     [
       "@semantic-release/commit-analyzer",
@@ -22,15 +22,14 @@ module.exports = {
     [
       "@semantic-release/changelog",
       {
-        changelogFile: "${MODULE_PATH}/CHANGELOG.md",
+        changelogFile: `${process.env.MODULE_PATH}/CHANGELOG.md`,
       },
     ],
     [
       "@semantic-release/git",
       {
-        assets: ["${MODULE_PATH}/CHANGELOG.md"],
-        message:
-          "chore(${MODULE_NAME}): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        assets: [`${process.env.MODULE_PATH}/CHANGELOG.md`],
+        message: `chore(${process.env.MODULE_NAME}): release \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}`,
       },
     ],
     [
