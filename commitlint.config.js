ce2698f356adad5274c793091a21c361c62378b6
Felipe Pimentel <fpimentel88@gmail.com>
2025-01-11 23:40:51 -0300

    fix(workflow): correct source path for ci cd

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["docs", "python", "nodejs", "ruby", "global", "n8n", "n8n-digitalocean", "digitalocean"]],
+    "scope-enum": [2, "always", ["docs", "workflow", "python", "nodejs", "ruby", "global", "n8n", "n8n-digitalocean", "digitalocean"]],
     "scope-empty": [2, "never"],
     "scope-case": [2, "always", "lower-case"],
     "subject-case": [2, "always", "lower-case"],
